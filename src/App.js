d724a59f976dca23b7052193e56db5f2a96ce327
afzah <afzaphaltankar@gmail.com>
2023-08-08 11:16:12 +0530

    This is project

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Login from './component/Login';
 import Home from './component/Home';
 import Signup from './component/Signup';
+import NewPassword from './component/NewPassword';
 function App() {
   return (
     <>
@@ -10,6 +11,8 @@ function App() {
         <Route path='/login' element={<Login/>}/>
         <Route path='/' element={<Home/>}/>
         <Route path='/signup' element={<Signup/>}/>
+        <Route path='/newpassword' element={<NewPassword/>}/>
+
 
         
       </Routes>
--- src/component/NewPassword.jsx
+++ src/component/NewPassword.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 
 
-const Login = () => {
+const NewPassword = () => {
   return (
     <div>
         <nav className="bg-blue-900 p-4">
@@ -13,6 +13,9 @@ const Login = () => {
         <div className="md:flex space-x-4">
         <Link to="/" className="text-white hover:underline">
             Home
+          </Link>
+          <Link to="/login" className="text-white hover:underline">
+            Login
           </Link>
           <Link to="/signup" className="text-white hover:underline">
             Signup
@@ -27,8 +30,7 @@ const Login = () => {
             Contact
           </Link>
           <Link to="/cart" className="text-white hover:underline">
-          <i class="fa-solid fa-cart-shopping" style={{color: "#f6f9fd",}}></i>
-          {/* <FontAwesomeIcon icon="fa-solid fa-cart-shopping" style={{color: "#f6f9fd",}} />           */}
+            Cart
           </Link>
         </div>
       </div>
@@ -39,33 +41,32 @@ const Login = () => {
       <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-white dark:border-gray-700">
           <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
               <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-black">
-                  Sign in to your account
+                  Forget Password
               </h1>
               <form class="space-y-4 md:space-y-6" action="#">
-                  <div>
-                      <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Your email</label>
-                      <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required=""/>
+              <div>
+                      <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Create new Password</label>
+                      <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
                   </div>
                   <div>
-                      <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Password</label>
+                      <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Confirm Password</label>
                       <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
                   </div>
                   <div class="flex items-center justify-between">
                       <div class="flex items-start">
-                          <div class="flex items-center h-5">
+                          {/* <div class="flex items-center h-5">
                             <input id="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required=""/>
-                          </div>
-                          <div class="ml-3 text-sm">
+                          </div> */}
+                          {/* <div class="ml-3 text-sm">
                             <label for="remember" class="text-gray-500 dark:text-black">Remember me</label>
-                          </div>
+                          </div> */}
                       </div>
                       {/* <a href="#" class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a> */}
-                      <Link to='/newpassword' class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</Link>
                   </div>
-                  <button type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
-                  <p class="text-sm font-light text-gray-500 dark:text-black">
-                      Don’t have an account yet? <Link to='/signup' class="font-medium text-primary-600 hover:underline dark:text-blue-500">Signup</Link>
-                  </p>
+                  <button type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Create</button>
+                  {/* <p class="text-sm font-light text-gray-500 dark:text-black">
+                      Don’t have an account yet? <a href="#" class="font-medium text-primary-600 hover:underline dark:text-blue-500">Sign up</a>
+                  </p> */}
               </form>
           </div>
       </div>
@@ -76,4 +77,4 @@ const Login = () => {
   )
 }
 
-export default Login
+export default NewPassword
